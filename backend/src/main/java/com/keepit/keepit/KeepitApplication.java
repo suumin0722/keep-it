a960f432de91f77bd1a687b8d561f824ed0fd77f
ohruha <85723624+ohruha@users.noreply.github.com>
2025-02-17 19:13:37 +0900

    Fix line endings to LF

---
+++ backend/src/main/java/com/keepit/keepit/KeepitApplication.java
@@ -0,0 +1,13 @@
+package com.keepit.keepit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepitApplication.class, args);
+	}
+
+}
