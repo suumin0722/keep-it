043ae651ba39bfc1e3fa9f4f175e121a9e2e1a53
ohruha <85723624+ohruha@users.noreply.github.com>
2025-02-19 01:37:02 +0900

    Add frontend project

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,18 +1,41 @@
-module.exports = [
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  { ignores: ['dist', 'node_modules'] },
   {
-    ignores: ['node_modules/', 'dist/']
-  },
-  {
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 'latest',
-      sourceType: 'module'
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
     },
+    settings: { react: { version: 'detect' } },
     plugins: {
-      prettier: require('eslint-plugin-prettier')
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
     },
     rules: {
-      'prettier/prettier': 'error',
-      'no-unused-vars': 'warn'
-    }
-  }
-]
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error', // Prettier 규칙 적용
+      'no-unused-vars': 'warn', // 사용하지 않는 변수 경고
+    },
+  },
+];
